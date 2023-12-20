5d752e7a6f15f457946443017b369634f35c9e9c
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2023-12-20 19:26:20 -0300

    adicionando chat de texto na tela de chat

--- front/src/Chat.js
+++ front/src/Chat.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import SimplePeer from "simple-peer";
+import { set } from "mongoose";
 
 const socket = io("http://localhost:8000/ws");
 
 const Chat = () => {
     const localVideoRef = useRef();
     const remoteVideoRef = useRef();
+    const [message, setMessage] = useState('');
+    const [response, setResponse] = useState('');
     let peer;
     let localStream;
 
     useEffect(() => {
+        socket.on("response", () => {
+            console.log(socket.id);
+            setResponse(response);
+        });
         return () => {
             if (peer) {
                 peer.destroy();
@@ -59,6 +66,11 @@ const Chat = () => {
         remoteVideoRef.current.srcObject = null;
     };
 
+    const sendChatMessage = (message) => {
+        // send chat message
+        socket.emit("chat_message", message);
+    }
+
     return (
         <div className="chat-container">
             <h1 className="title">Video Chat</h1>
@@ -66,6 +78,19 @@ const Chat = () => {
             <video ref={remoteVideoRef} autoPlay></video>
             <button onClick={startVideoChat}>Start Video Chat</button>
             <button onClick={stopVideoChat}>Stop Video Chat</button>
+            <div className="text-chat">
+                <h1 className="title">Text Chat</h1>
+                <div className="chat-box">
+                    <div className="chat-messages"></div>
+                    <form className="chat-form">
+                        <input type="text" placeholder="Type your message..." value={message} onChange={(e) => setMessage(e.target.value)}/>
+                        <button type="submit" onClick={sendChatMessage(message)}>Send</button>
+                    </form>
+                    <div className="chat-messages">
+                        <p className="chat-message">{response}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
