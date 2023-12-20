8f3a23eacae2ccf984b77a94ff5c5cf62f7b1e37
Fernando Vargas <nandovargas7@gmail.com>
2023-12-20 03:20:06 -0300

    Criacao do Dockefile e inclusao no compose

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost/serv_dialogo"
+# SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@localhost/serv_dialogo"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db/serv_dialogo"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
