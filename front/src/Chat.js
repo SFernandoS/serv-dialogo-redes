d4d695e515ad1eb283495737c07818180dc4ad61
Fernando Vargas <nandovargas7@gmail.com>
2023-12-20 22:24:03 -0300

    Mudancas front

--- front/src/Chat.js
+++ front/src/Chat.js
@@ -1,39 +1,49 @@
 import React, { useEffect, useRef, useState } from "react";
-import io from "socket.io-client";
-import SimplePeer from "simple-peer";
 import { useParams } from "react-router-dom";
+import SimplePeer from "simple-peer";
 
 const Chat = () => {
-    const userId = useParams().userId;
-    const topicId = useParams().topicId;
-    const socket = io(`http://localhost:8000/ws/topic/${userId}/${topicId}}`);
+    const { userId, topicId } = useParams();
+    const websocket = useRef(null);
     const localVideoRef = useRef();
     const remoteVideoRef = useRef();
     const [message, setMessage] = useState('');
-    const [response, setResponse] = useState('');
+    const [chatLog, setChatLog] = useState([]);
     const [peer, setPeer] = useState(null);
     const [localStream, setLocalStream] = useState(null);
     const [remoteStream, setRemoteStream] = useState(null);
 
+    const openDialog = () => {
+        setShowDialog(true);
+    };
+    
+    const closeDialog = () => {
+        setShowDialog(false);
+    };
+
     useEffect(() => {
-        const handleResponse = (data) => {
-            console.log(socket.id);
-            setResponse(data);
+        websocket.current = new WebSocket(`ws://localhost:8000/ws/topic/${userId}/${topicId}`);
+        websocket.current.onmessage = event => {
+            try {
+                const data = JSON.parse(event.data);
+                setChatLog(prevLog => [...prevLog, `Recebido: ${data.message}`]);
+            } catch (error) {
+                console.error("Erro ao analisar a mensagem do WebSocket:", error);
+            }
         };
-
-        socket.on("response", handleResponse);
-
         return () => {
-            if (peer) {
-                peer.destroy();
-            }
-            if (localStream) {
-                localStream.getTracks().forEach((track) => track.stop());
-            }
-            // Remove the event listener when the component unmounts
-            socket.off("response", handleResponse);
+            if (websocket.current) websocket.current.close();
         };
-    }, [peer, localStream]); // Add 'response' to the dependencies if needed
+    }, [userId, topicId]);
+
+    const sendChatMessage = () => {
+        if (websocket.current && message.trim()) {
+            const messageData = JSON.stringify({ message });
+            websocket.current.send(messageData);
+            setChatLog(prevLog => [...prevLog, `Você: ${message}`]);
+            setMessage('');
+        }
+    };
 
     const startVideoChat = async () => {
         try {
@@ -44,7 +54,7 @@ const Chat = () => {
             const newPeer = new SimplePeer({ initiator: false, stream });
 
             newPeer.on("signal", (data) => {
-                socket.emit("video_answer", data);
+                websocket.current.emit("video_answer", data);
             });
 
             newPeer.on("stream", (remoteStream) => {
@@ -52,11 +62,11 @@ const Chat = () => {
                 remoteVideoRef.current.srcObject = remoteStream;
             });
 
-            socket.on("video_offer", (data) => {
+            websocket.current.on("video_offer", (data) => {
                 newPeer.signal(data);
             });
 
-            socket.on("ice_candidate", (data) => {
+            websocket.current.on("ice_candidate", (data) => {
                 newPeer.signal(data);
             });
 
@@ -79,38 +89,44 @@ const Chat = () => {
         remoteVideoRef.current.srcObject = null;
     };
 
-    const sendChatMessage = (message) => {
-        // send chat message
-        socket.emit("chat_message", message);
-    };
-
     return (
         <div className="chat-container">
-            <h1 className="title">Video Chat</h1>
-            <video ref={localVideoRef} autoPlay muted></video>
-            <video ref={remoteVideoRef} autoPlay></video>
-            <button onClick={startVideoChat}>Start Video Chat</button>
-            <button onClick={stopVideoChat}>Stop Video Chat</button>
-            <div className="text-chat">
-                <h1 className="title">Text Chat</h1>
-                <div className="chat-box">
-                    <div className="chat-messages"></div>
-                    <form
-                        className="chat-form"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            sendChatMessage(message);
-                            setMessage('');
-                        }}
-                    >
-                        <input type="text" placeholder="Type your message..." value={message} onChange={(e) => setMessage(e.target.value)} />
-                        <button type="submit">Send</button>
-                    </form>
-                    <div className="chat-messages">
-                        <p className="chat-message">{response}</p>
-                    </div>
-                </div>
+            {/* Área de Vídeo */}
+            <div className="video-chat">
+                <video ref={localVideoRef} autoPlay muted></video>
+                <video ref={remoteVideoRef} autoPlay></video>
+                <button onClick={startVideoChat}>Start Video Chat</button>
+                <button onClick={stopVideoChat}>Stop Video Chat</button>
             </div>
+
+            {/* Área de Chat de Texto */}
+            <div className="text-chat">
+                <h2>Text Chat</h2>
+                <div className="chat-messages-box">
+                    {chatLog.map((msg, index) => (
+                        <p key={index} className="chat-message">{msg}</p>
+                    ))}
+                </div>
+                <form className="chat-form" onSubmit={e => {
+                    e.preventDefault();
+                    sendChatMessage();
+                }}>
+                    <input
+                        type="text"
+                        placeholder="Type your message..."
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+
+            {showDialog && (
+                <div className="dialog-box">
+                    <p>Mensagem enviada com sucesso!</p>
+                    <button onClick={closeDialog}>Fechar</button>
+                </div>
+            )}
         </div>
     );
 };
